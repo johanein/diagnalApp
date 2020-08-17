cb18e35f323ef946aa20edb8b7fb853f5db94400
Albert <johanein@gmail.com>
2020-08-17 23:56:15 +0530

    Updates

---
+++ precache-manifest.d1016b88da285c11ede879eb6d759631.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e42feefabfe9807f0f3aa7b6f6388434",
+    "url": "/diagnalApp/index.html"
+  },
+  {
+    "revision": "39ce0cb7fb4e5419f229",
+    "url": "/diagnalApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "930d1734c131c537784d",
+    "url": "/diagnalApp/static/js/2.77e86286.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/diagnalApp/static/js/2.77e86286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ce0cb7fb4e5419f229",
+    "url": "/diagnalApp/static/js/main.dec49740.chunk.js"
+  },
+  {
+    "revision": "2f04d27d0f70a7a26687",
+    "url": "/diagnalApp/static/js/runtime-main.75f90173.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/diagnalApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
