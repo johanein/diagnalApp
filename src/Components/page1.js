e64db754417091dfb6bfc6aa7adf47fe69ff3e0f
Albert <johanein@gmail.com>
2020-08-20 02:42:33 +0530

    Grid added with scroll and pics from json

--- src/Components/page1.js
+++ src/Components/page1.js
@@ -1,24 +1,61 @@
 import React, { memo, useEffect } from 'react'
 import { jsonPlaceholderGetReq } from '../Redux/actions/jsonPlaceHolderAction'
+import { makeStyles } from '@material-ui/core/styles';
+import {Grid} from '@material-ui/core';
 
 import {  useSelector, useDispatch } from 'react-redux'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    alignItems:'center',
+    overflow: 'auto',
+    // backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: '60%',
+    height: '80%',
+  }
+}));
+
 const Page1 = memo(function Page1(props) {
+  const classes = useStyles();
     const dispatch = useDispatch()
     const jsonAxiosGetResult = useSelector(state => state.jsonPlaceHolderReducer.jsonAxiosGetResult) 
-    const {page:{title}={}} = jsonAxiosGetResult
+    const {page:{'content-items':{content=[]}={}}={}} = jsonAxiosGetResult
   
     useEffect(() => {
       dispatch(jsonPlaceholderGetReq())
     }, [])
+    console.log('content : ',content)
 
     return (
-        <>
-           Hello !
-      {title} 
-        </>
-    )
+      <div className={classes.root}>
+        <Grid 
+        container 
+        direction="row"
+        // justify="center" 
+        alignItems="center"
+        className={classes.gridList} 
+        // spacing={2}
+        // cols={3}
+        >
+        {content.map((tile,index) => (
+          <Grid key={`${index}_${tile.name}`}
+          item
+          xs={4}
+          >
+            <img src={`${process.env.PUBLIC_URL}/Slices/${tile['poster-image']}`} alt={tile.name} />
+          <h3>{tile.name}</h3>
+          </Grid>
+        ))}
+      </Grid>
+      </div>
+    );
 })
+
 
 
 export default Page1
--- src/Redux/actions/jsonPlaceHolderAction.js
+++ src/Redux/actions/jsonPlaceHolderAction.js
@@ -2,25 +2,25 @@
 import {
     SAVE_JSON_PLACEHOLDER
 } from '../constants'
-import page1 from '../JsonFolder/page1';
 
-// import network from './network'
+import network from './network'
 
  const jsonPlaceholderActionCreator = (payload) => ({
     type:SAVE_JSON_PLACEHOLDER,
     payload
 })
 
-// const netWorkFunction = async ({url}) => {
-//     const response = await network.get(url)
-// return response
-// }
+const netWorkFunction = async ({url}) => {
+    const response = await network.get(url)
+return response
+}
 
  const jsonPlaceholderGetReq = () =>{
-    return (dispatch)=>{
-        // const payload = await page1.json()
-        console.log('page1', page1)
-        dispatch(jsonPlaceholderActionCreator(page1))
+    return async (dispatch)=>{
+        const payload =  await netWorkFunction({url:'API/CONTENTLISTINGPAGE-PAGE1.json'})
+        console.log('payload', payload)
+        const {data} = payload
+        dispatch(jsonPlaceholderActionCreator(data))
     }  
 }
 
