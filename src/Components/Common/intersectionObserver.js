e21b082f9228e8697e990b4e9ec27f025e3e32da
albert.c <albert.c@geservs.com>
2021-01-21 22:01:25 +0530

    added custom intersection observer

---
+++ src/Components/Common/intersectionObserver.js
@@ -0,0 +1,24 @@
+import { useEffect, useRef, useState } from 'react';
+
+function intersectionObserver(options) {
+  const ref = useRef();
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setVisible(entry.isIntersecting);
+    }, options);
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref, options]);
+
+  return [ref, visible];
+}
+
+export default intersectionObserver;
